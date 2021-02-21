1adb1128d9ef09dd6619dd54245945b87148335c
alikoroglu <48138710+alikoroglu@users.noreply.github.com>
2021-02-21 12:12:59 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/Dictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class Dictionary<keyT,valT>
+    {
+        keyT[] myDictionaryKeys;
+        keyT[] tempKeys;
+        valT[] myDictionaryVals;
+        valT[] tempVals;
+
+        public Dictionary()
+        {
+            myDictionaryKeys = new keyT[0];
+            myDictionaryVals = new valT[0];
+        }
+
+        public void Add(keyT K,valT V)
+        {
+            tempKeys = myDictionaryKeys;
+            tempVals = myDictionaryVals;
+            myDictionaryKeys = new keyT[tempKeys.Length + 1];
+            myDictionaryVals = new valT[tempVals.Length + 1];
+            for (int i = 0; i <tempKeys.Length; i++)
+            {
+                myDictionaryKeys[i] = tempKeys[i];
+                myDictionaryVals[i] = tempVals[i];
+            }
+            myDictionaryKeys[myDictionaryKeys.Length - 1] = K;
+            myDictionaryVals[myDictionaryVals.Length - 1] = V;
+        }
+
+        public int Count
+        {
+            get { return myDictionaryKeys.Length; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> sehirler = new Dictionary<int, string>();
+
+            sehirler.Add(52, "Ordu");
+            sehirler.Add(34, "Istanbul");
+
+            Console.WriteLine(sehirler.Count);
+
+
+        }
+    }
+}
